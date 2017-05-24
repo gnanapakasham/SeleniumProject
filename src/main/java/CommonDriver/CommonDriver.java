27690ec4f313c3e60517f7ca95a13fd0a6318d80
sgnanaprakasham <sgprakash247@gmail.com>
2017-05-24 08:52:46 +0530

    project

--- src/main/java/CommonDriver/CommonDriver.java
+++ src/main/java/CommonDriver/CommonDriver.java
@@ -1,29 +1,22 @@
 package CommonDriver;
 
-import java.io.File;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CommonDriver {
-
 
 	private WebDriver Driver;
 	private long setPageLoadTimmeout;
 	private long elementDetectionTimeout;
 
+	public static Logger LOGGER = Logger.getLogger(CommonDriver.class.getName());
+
 	public CommonDriver() {
-		
+
 		setPageLoadTimmeout = 90l;
 		elementDetectionTimeout = 40l;
 	}
@@ -35,29 +28,38 @@ public class CommonDriver {
 	public void setElementDetectionTimeout(long elementDetectionTimeout) {
 		this.elementDetectionTimeout = elementDetectionTimeout;
 	}
-	
-	public void invokeBrowser (String browserType, String url) throws Exception{
-		
-		browserType= browserType.trim();
-		url = url.trim();
-		
-		if(browserType.equalsIgnoreCase("chrome") || browserType.equalsIgnoreCase("google")|| browserType.equalsIgnoreCase("google chrome")){
-			
-			System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\chromedriver.exe");
-			
-			Driver = new ChromeDriver();
-		}else if(browserType.equalsIgnoreCase("mozilla") || browserType.equalsIgnoreCase("firefox")|| browserType.equalsIgnoreCase("mozilla firefox")){
-			
-			System.setProperty("webdriver.gekodriver.driver", "");
-			
-			Driver = new FirefoxDriver();
-		}else{
-			
-			throw new Exception("Invalid Browser Type : "+ browserType);
-			}
-		}
-	
-	
-	
-}
 
+	public void invokeBrowser(String browserType, String url) throws Exception {
+
+		browserType = browserType.trim();
+		url = url.trim();
+
+		if (browserType.equalsIgnoreCase("chrome") || browserType.equalsIgnoreCase("google")
+				|| browserType.equalsIgnoreCase("google chrome")) {
+
+			LOGGER.info("Set chrome driver path and initialize chrome driver");
+
+			System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\chromedriver.exe");
+
+			Driver = new ChromeDriver();
+		} else if (browserType.equalsIgnoreCase("mozilla") || browserType.equalsIgnoreCase("firefox")
+				|| browserType.equalsIgnoreCase("mozilla firefox")) {
+
+			LOGGER.info("Set chrome driver path and initialize chrome driver");
+			System.setProperty("webdriver.gekodriver.driver", "");
+
+			Driver = new FirefoxDriver();
+		} else {
+
+			throw new Exception("Invalid Browser Type : " + browserType);
+		}
+
+		Driver.manage().window().maximize();
+		Driver.manage().deleteAllCookies();
+		Driver.manage().timeouts().setScriptTimeout(setPageLoadTimmeout, TimeUnit.SECONDS);
+		Driver.manage().timeouts().setScriptTimeout(elementDetectionTimeout, TimeUnit.SECONDS);
+		Driver.get(url);
+
+	}
+
+}
